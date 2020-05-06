237034675a1de5ea27c53bc20b0cf752bf4702c1
pzcend <vigourouxpz@gmail.com>
2020-05-06 18:28:13 +0200

    new version 0605-v3

--- static/mail.php
+++ static/mail.php
@@ -3,22 +3,23 @@ $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
 $captcha = $_POST['captcha'];
+
+
+if($captcha === 'OPureCreation'){ 
+
+$reponse ="Confirmation de la reception de votre message" ;
+$retour ="Merci pour votre message, nous vous recontacterons dans les meilleurs délais.";
+
 $formcontent = "From: $name \n Message: $message";
 $recipient = "contact@opurecreation.com";
 $subject = "Formulaire de contact new site";
 $mailheader = "From: $email \r\n";
-if($captcha === 'OPureCreation'){ 
-	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-
-	$reponse ="Confirmation de la reception de votre message" ;
-	$retour ="Merci pour votre message, nous vous recontacterons dans les meilleurs délais.";
-	$bouton ="<a class="btn btn-primary btn-lg" href="/" role="button">Retour à l'accueil</a>";
+mail($recipient, $subject, $formcontent, $mailheader);
 
 }else{
 
 $reponse ="Merci de valider le captcha avant d'envoyer votre message";
 $retour ="";
-$bouton ="<a class="btn btn-primary btn-lg" href="/contact" role="button">Retour à la page contact</a>";
 }
 
 
@@ -35,7 +36,7 @@ $bouton ="<a class="btn btn-primary btn-lg" href="/contact" role="button">Retour
 				<div style="margin-top:150px">
 				<div class="jumbotron">
   <h1 class="display-4"><?php echo $reponse; ?></h1>
-  <p class="lead"><?php echo $retour; ?></p>
+  <p class="lead"><a class="btn btn-primary btn-lg" href="/contact" role="button">Retour à la page contact</a></p>
   <hr class="my-4">
   <?= $bouton ?>
 </div>
